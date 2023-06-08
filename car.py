263cc1c402b639c4994c390b29b7e624f4080e75
Amacy Lee <davidamacylee@gmail.com>
2023-06-08 13:52:53 -0700

    first commit

---
+++ car.py
@@ -0,0 +1,10 @@
+class Car():
+    wheels = 4
+    color = 'blue'
+    
+    def headlights():
+        print('Turn them on for safety, do not crash into a moose.')
+
+ford = Car()
+
+ford.headlights()
